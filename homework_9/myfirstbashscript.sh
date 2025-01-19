28983e54cdf017c038f9a12f6ceb949f948959c7
meni0419 <meni0419@gmail.com>
2025-01-19 21:18:24 +0200

    added hw 9

---
+++ homework_9/myfirstbashscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ANSI escape codes for text colors
+RED="\033[31m"
+GREEN="\033[32m"
+BLUE="\033[34m"
+RESET="\033[0m"  # Resets the text color to default
+USER=MaksumM
+# Use tee to display colorized output and log plain text simultaneously
+date
+printf "Hello ${GREEN}%s${RESET}!\n" "$USER"
+printf "Script run from ${BLUE}%s${RESET}!\n" "$(pwd)"
+ps aux | grep bioset | grep -v grep | wc -l
+ls -lah /etc/ | awk ' $9 == "passwd" {print $1}'
