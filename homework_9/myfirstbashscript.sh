2dde7fcf1c605bdbb9a7693350c7d3d52e282a1a
meni0419 <meni0419@gmail.com>
2025-01-20 07:37:48 +0200

    hw 10 gitignore changed

--- homework_9/myfirstbashscript.sh
+++ homework_9/myfirstbashscript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # ANSI escape codes for text colors
-RED="\033[31m"
 GREEN="\033[32m"
 BLUE="\033[34m"
 RESET="\033[0m"  # Resets the text color to default
@@ -10,4 +9,4 @@ date
 printf "Hello ${GREEN}%s${RESET}!\n" "$USER"
 printf "Script run from ${BLUE}%s${RESET}!\n" "$(pwd)"
 ps aux | grep bioset | grep -v grep | wc -l
-ls -lah /etc/ | awk ' $9 == "passwd" {print $1}'
+ls -lah /etc/ | awk '$9 == "passwd" {print $1}'
